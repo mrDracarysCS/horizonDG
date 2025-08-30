3f64aeeb8ad1ef94e126e14a82bdea9f7a927dd5
mr_JaimePerez <jaime43lenin@gmail.com>
2025-08-30 14:16:30 -0400

    Vite base no needed

--- horizon-node-react/vite.config.js
+++ horizon-node-react/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'horizonDG',   // <-- add this if your URL is https://yourname.github.io/<YOUR_REPO>/
+  
 })
